6bde935b84a19751047d6b13c508f4ea977d6f29
Sergey Soldatov <angel-cc@yandex.ru>
2022-01-22 19:39:50 +0300

    feat(*)

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,13 @@
 import express, { Express } from 'express';
-import 'express-async-errors';
+import mongoose from 'mongoose';
 
 import { json } from 'body-parser';
 
 import { currentUserRouter, signInRouter, signOutRouter, signUpRouter } from './routes';
 import { errorHandler } from './middleware/error-handler';
 import { NotFoundError } from './errors/not-found-error';
+
+import 'express-async-errors';
 
 const app: Express = express();
 
@@ -22,6 +24,18 @@ app.all('*', async (req, res, next) => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!!!!!');
-});
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('Connected to DB');
+  } catch (e) {
+    console.error(e);
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!!!!');
+  });
+}
+
+start();
+
